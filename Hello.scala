25fa0842259ab6bf047135e1c518066f98fff61a
Xuebing Li <xuebinglee@gmail.com>
2024-02-27 14:47:47 +0900

    Add formatter

--- Hello.scala
+++ Hello.scala
@@ -2,7 +2,7 @@
 //> using dep com.lihaoyi::os-lib:0.9.3
 
 object Hello:
-  def greeting(name: String): String = 
+  def greeting(name: String): String =
     s"Hello, ${name}!"
-    
+
   @main def main() = println(greeting("world"))
